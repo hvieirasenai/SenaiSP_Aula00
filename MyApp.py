019632674001447fe7d32403c21d2934dfbecb01
Henrihva <h.vieira.senai@gmail.com>
2024-01-15 18:21:45 -0300

    Correção match
    Correção match

--- MyApp.py
+++ MyApp.py
@@ -13,28 +13,22 @@ arquivo = st.file_uploader(
     type=['jpg', 'png', 'py', 'wav', 'mp3', 'csv', 'json','mp4', 'mpeg']
 )
 
-# st.text_input('Email', max_chars=10)
-# st.text_input('Senha', type='password')
-
-
 if arquivo is not None:
-    # print(arquivo.type)
-    # match arquivo.type.split('/'):
     tipo_arquivo = arquivo.type.split('/')
-    match tipo_arquivo:        
-        case 'application', 'json':
+    match tipo_arquivo:
+        case ['application', 'json']:
             st.json(loads(arquivo.read()))
-        case 'image', _:
+        case ['image', _]:
             st.image(arquivo)
-        case 'text', 'csv':
+        case ['text', 'csv']:
             df = read_csv(arquivo).transpose()
             st.dataframe(df)
             st.bar_chart(df)
-        case 'text', 'x-python':
+        case ['text', 'x-python']:
             st.code(arquivo.read().decode())
-        case 'audio', _:
+        case ['audio', _]:
             st.audio(arquivo)
-        case 'video', _:
+        case ['video', _]:
             st.video(arquivo)
 else:
     st.error('Ainda não tenho arquivo!')
